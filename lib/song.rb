79abffdf48ad5684ca999242c077990e91a4b50a
Monica Marziano <mmarziano1@gmail.com>
2018-05-05 11:09:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   
-  @@count = 0 
+  @@count = 0
+  @@artist = []
+  @@genre = []
   
   def initialize(name, artist, genre)
     @@count += 1 

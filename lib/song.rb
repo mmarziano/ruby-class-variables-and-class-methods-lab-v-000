2b9c1fc66e627fa1add71861df44b7e791e24ed6
Monica Marziano <mmarziano1@gmail.com>
2018-05-05 11:27:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@genre_count = Hash.new
-  @@artist_count = Hash.new
+  @@genre_count = Hash.new(0)
+  @@artist_count = Hash.new(0)
   
   def initialize(name, artist, genre)
     @@count += 1 
